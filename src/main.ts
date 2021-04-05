18b7e136e6b8327e682a166b247d72f1ab4c1658
Alexey <58986485+rielyalkov@users.noreply.github.com>
2021-04-05 15:09:08 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import {kekjs} from '../kek.js';
+
+console.log(kekjs);
 
 if (environment.production) {
   enableProdMode();
